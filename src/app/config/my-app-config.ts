77e9d6cd387811a1038b00d4cab907ff3e53b083
Abc69707753 <wilson_mak123@hotmail.com>
2022-05-10 23:53:32 +0800

    openssl TSL establishment HTTPS config

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default  {
     oidc:{
         clientId: '0oa4ukfhgjmJcVI1N5d7',
         issuer:'https://dev-41106686.okta.com/oauth2/default',
-        redirectUri:'http://localhost:4200/login/callback',
+        redirectUri:'https://localhost:4200/login/callback',
         scopes:['openid','profile','email']
     }
 }
