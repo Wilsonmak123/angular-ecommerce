7a633147eafcf6ba0c4e65c86da6c9e5b23a79ce
Abc69707753 <wilson_mak123@hotmail.com>
2022-05-17 09:38:05 +0800

    isDisabled boolean to avoid multiple click on purchase button

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -45,6 +45,8 @@ export class CheckoutComponent implements OnInit {
   cardElement: any;
   displayError: any = "";
 
+  isDisabled: boolean = false;
+
 
   constructor(private formBuilder: FormBuilder,
     private shoppingKingFormService: ShoppingKingFormService,
@@ -248,6 +250,7 @@ export class CheckoutComponent implements OnInit {
     // - place order
 
     if (!this.checkoutFormGroup.invalid && this.displayError.textContent === "") {
+      this.isDisabled = true
       this.checkoutService.createPaymentIntent(this.paymentInfo).subscribe(
         (paymentIntentResponse) => {
           this.stripe.confirmCardPayment(paymentIntentResponse.client_secret,
@@ -271,6 +274,7 @@ export class CheckoutComponent implements OnInit {
               if (result.error) {
                 /// inform the customer there was an error
                 alert(`There was an error: ${result.error.message}`);
+                this.isDisabled = false;
               } else {
                 // call REST API via CheckoutService
                 this.checkoutService.placeOrder(purchase).subscribe({
@@ -279,9 +283,12 @@ export class CheckoutComponent implements OnInit {
 
                     // reset card
                     this.resetCart();
+                    this.isDisabled = false;
                   },
                   error: err => {
                     alert(`There wan an error: ${err.message}`);
+                    this.isDisabled = false;
+
                   }
                 })
               }
